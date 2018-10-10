478be03e12090a2769f4156cc240e26d9112c95f
dkrnl <dkrnl@yandex.ru>
2018-10-10 09:25:57 +0700

    update postcss to 7

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-font-display', function (options) {
-    options = options || { display: 'swap', replace: false };
+  options = options || { display: 'swap', replace: false }
 
-    return function (root) {
+  return function (root) {
+    root.walkAtRules('font-face', function (rule) {
+      var exists = false
+      rule.walkDecls('font-display', function (decl) {
+        if (options.replace) {
+          decl.value = options.display
+        }
+        exists = true
+      })
 
-        root.walkAtRules('font-face', (rule) => {
-
-            var exists = false;
-            rule.walkDecls('font-display', (decl) => {
-                if (options.replace) {
-                    decl.value = options.display;
-                }
-                exists = true;
-            });
-
-            if (!exists) {
-                rule.append({ prop: 'font-display', value: options.display });
-            }
-
-        });
-
-    };
-});
+      if (!exists) {
+        rule.append({ prop: 'font-display', value: options.display })
+      }
+    })
+  }
+})
--- index.test.js
+++ index.test.js
@@ -1,30 +1,35 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
-var plugin = require('./');
+var plugin = require('./')
 
-function run(input, output, options) {
-    return postcss([ plugin(options) ]).process(input, { from: undefined })
-        .then(result => {
-            expect(result.css.replace(/\s+/g, ' ')).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+function run (input, output, options) {
+  return postcss([plugin(options)]).process(input, { from: undefined })
+    .then(function (result) {
+      expect(result.css.replace(/\s+/g, ' ')).toEqual(output)
+      expect(result.warnings()).toHaveLength(0)
+    })
 }
 
-it('add new font-display', () => run(
+it('add new font-display', function () {
+  return run(
     '@font-face { font-family: \'My Font\'; }',
     '@font-face { font-family: \'My Font\'; font-display: swap; }',
     { display: 'swap' }
-));
+  )
+})
 
-it('pass exists font-display', () => run(
+it('pass exists font-display', function () {
+  return run(
     '@font-face { font-family: \'My Font\'; font-display: auto; }',
     '@font-face { font-family: \'My Font\'; font-display: auto; }',
     { display: 'swap', replace: false }
-));
+  )
+})
 
-it('replace exists font-display', () => run(
+it('replace exists font-display', function () {
+  return run(
     '@font-face { font-family: \'My Font\'; font-display: auto; }',
     '@font-face { font-family: \'My Font\'; font-display: swap; }',
     { display: 'swap', replace: true }
-));
-
+  )
+})
